059afbf092947bfd7a95ce80867d5bb1f0aebb60
Benjamin Ogles <benjaminogles@gmail.com>
2022-06-29 16:46:05 -0400

    more work on the background section

--- src/posts/dft/02-background.py
+++ src/posts/dft/02-background.py
@@ -5,9 +5,10 @@
 # -----------------------
 #
 # Complex numbers have the form `x + jy` where the number `x` is called the real part, the number `y` is called the imaginary part and `j` has the strange property that `j * j = -1`.
-# Plotting complex numbers as points on a 2D plane gives a graphical view of how their magnitude and phase are defined.
-# The length of the line between the origin and the point is the number's magnitude.
-# The angle between the positive real axis and the line is the number's phase.
+# Plotting the real and imaginary parts as a point on a 2D plane gives a graphical view of how the number's magnitude and phase is defined.
+# The length of the line connecting the origin and the point is the number's magnitude.
+# The angle between the positive real axis and that line is the number's phase.
+# The sign of the number's phase indicates whether it is measured in a clockwise (positive) or counter-clockwise (negative) direction.
 #
 # lit skip
 
@@ -16,7 +17,7 @@ import matplotlib.patches as patches
 import numpy as np
 
 plt.plot([0, 1], [0, 1])
-plt.text(.5, .6, '$r$')
+plt.text(.5, .6, '$R$')
 plt.plot(1, 1, '.', markersize=10)
 plt.text(1, 1.1, '$(x,y)$')
 plt.plot([0, 1], [0, 0])
@@ -38,3 +39,14 @@ plt.close()
 
 # lit execute
 # lit unskip
+# lit text
+#
+# As indicated in the figure, `x` and `y` can be rewritten in terms of the trigonometric functions `sin` and `cos`.
+# This gives us a way to write `x + jy` in terms of its magnitude and phase explicitly: `R(cos(θ) + jsin(θ))`.
+# We can also keep the explicit expression of magnitude and phase without using trigonometric functions by rewriting according to Euler's formula: `cos(θ) + jsin(θ) = exp(jθ)`.
+# Any complex number with magnitude `R` and phase `θ` can be written as `Rexp(jθ)`.
+#
+# Many common operations on complex numbers are simple to perform and understand in this exponential form.
+# Multiplying two complex numbers will multiply their magnitudes and add their phases e.g. `Aexp(jθ) * Bexp(jϕ) = ABexp(j(θ + ϕ))`.
+# Complex conjugation simply negates the phase of a number e.g `conj(Aexp(jθ)) = Aexp(-jθ)`.
+# So multiplying a complex number by its own conjugate always results in a scalar value equal to its magnitude squared since the phase goes to `0` and reduces the exponential to `1`.
