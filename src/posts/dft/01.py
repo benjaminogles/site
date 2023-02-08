7e50d04d75648eabbafa8742a2057efd10b6809e
Benjamin Ogles <benjaminogles@gmail.com>
2023-02-07 20:46:08 -0500

    start

---
+++ src/posts/dft/01.py
@@ -0,0 +1,10 @@
+
+# The Discrete Fourier Transform (DFT)
+# ====================================
+#
+# In this post, I derive the DFT by focusing on its relationship with the convolution operator.
+#
+# Convolution
+# -----------
+# One of the simplest ways to understand convolution is to see how it c;
+#
